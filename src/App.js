2ae1ac16247faaa2266e0c2f25ccd405f4b29e3a
Gina Sala <georgina.sala18@gmail.com>
2021-08-30 14:23:04 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
         <Search />
         <Weather />
         <Forecast />
         <Footer />
+      </div>
     </div>
   );
 }
