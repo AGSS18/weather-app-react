38eb987ca9e7ff80d9fa3d005f689e975698ce9f
Gina Sala <georgina.sala18@gmail.com>
2021-09-05 21:33:10 -0500

    background image change semi-working

--- src/Weather.js
+++ src/Weather.js
@@ -1,32 +1,32 @@
 import React, {useState} from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
-
 import "./Weather.css";
 
 export default function Weather(props) {
+ 
   const [weatherData, setWeatherData] = useState({ready: false});
   const [city, setCity] = useState(props.defaultCity);
   const [hidden, setHidden] = useState(true);
   const [favCities, setFavCities] = useState("");
-
+  
   function search() {
     const apiKey = "2a2676887289368652de121a9db03637";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  
+    
     axios.get(apiUrl).then(handleResponse);
   }
-
+  
   function handleSubmit(event) {
     event.preventDefault();
     search();
   }
-
+  
+  
   function handleCityChange(event) {
     setCity(event.target.value);
-
   }
-
+  
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -39,7 +39,11 @@ export default function Weather(props) {
       mainDescription: response.data.weather[0].main,
     });
   }
-
+  
+  function handleFavCity(event) {
+    search(event.target.innerHTML);
+  }
+  
   function showFavCities(event) {
     event.preventDefault();
     if(hidden === true) {
@@ -47,11 +51,11 @@ export default function Weather(props) {
       setHidden(false);
     } else {
       setFavCities(<ul className="favorites" id="favorites">
-            <li className="city-one">Monterrey</li>
-            <li className="city-two">Cancún</li>
-            <li className="city-three">Guadalajara</li>
-            <li className="city-four">New York</li>
-            <li className="city-five">San Antonio</li>
+            <li onClick={handleFavCity} className="city-one">Monterrey</li>
+            <li onClick={handleFavCity} className="city-two">Cancún</li>
+            <li onClick={handleFavCity} className="city-three">Guadalajara</li>
+            <li onClick={handleFavCity} className="city-four">New York</li>
+            <li onClick={handleFavCity} className="city-five">San Antonio</li>
           </ul>);
       setHidden(true);   
     }
