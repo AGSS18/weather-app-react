dc087b3c4cd5b86bc9ab01c9b39aca3e55a3b0f4
Gina Sala <georgina.sala18@gmail.com>
2021-09-05 21:39:12 -0500

    commented background image code for auto changing

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,45 +3,45 @@ import HandleMainIcon from "./HandleMainIcon";
 import UnitsConvertion from "./UnitsConvertion";
 
 export default function WeatherInfo(props){
-  const changeBgImage = {
-    multipleConditions: ["Mist", "Smoke", "Haze", "Dust", "Fog", "Sand", "Ash", "Squal",
-    "Tornado"],
-    otherConditions: ["Clear", "Clouds", "Thunderstorm", "Snow", "Drizzle", "Rain"],
-    multipleConditionsImages: [`mist.jpg`, `mist-2.jpg`, `mist-3.jfif`, `mist-4.jfif`, `mist-5.jfif`],
-    otherConditionsImages: {
-      "Clear" : [`sunny.jpg`, `sunny_2.jfif`, `sunny_3.jfif`, `sunny_4.jfif`, `sunny_5.jfif`],
-      "Clouds" : [`cloud.jpg`, `cloud_2.jfif`, `cloud_3.jfif`, `cloud_4.jfif`, `cloud_5.jfif`],
-      "Thunderstorm" : [`thunder.jfif`, `thunder_2.jfif`, `thunder_3.jfif`, `thunder_4.jfif`, `thunder_5.jfif`],
-      "Snow" : [`snowy.jpeg`, `snowy_2.jfif`, `snowy_3.jfif`, `snowy_4.jfif`, `snowy_5.jfif`],
-      "Drizzle" : [`drizzle.jfif`, `drizzle_2.jpg`, `drizzle_3.jfif`, `drizzle_4.jfif`, `drizzle_5.jfif`],
-      "Rain" : [`rain.jfif`, `rain_2.jfif`, `rain_3.jfif`, `rain_4.jfif`, `rain_5.jfif`],
-    }
-  }
+  // const changeBgImage = {
+  //   multipleConditions: ["Mist", "Smoke", "Haze", "Dust", "Fog", "Sand", "Ash", "Squal",
+  //   "Tornado"],
+  //   otherConditions: ["Clear", "Clouds", "Thunderstorm", "Snow", "Drizzle", "Rain"],
+  //   multipleConditionsImages: [`mist.jpg`, `mist-2.jpg`, `mist-3.jfif`, `mist-4.jfif`, `mist-5.jfif`],
+  //   otherConditionsImages: {
+  //     "Clear" : [`sunny.jpg`, `sunny_2.jfif`, `sunny_3.jfif`, `sunny_4.jfif`, `sunny_5.jfif`],
+  //     "Clouds" : [`cloud.jpg`, `cloud_2.jfif`, `cloud_3.jfif`, `cloud_4.jfif`, `cloud_5.jfif`],
+  //     "Thunderstorm" : [`thunder.jfif`, `thunder_2.jfif`, `thunder_3.jfif`, `thunder_4.jfif`, `thunder_5.jfif`],
+  //     "Snow" : [`snowy.jpeg`, `snowy_2.jfif`, `snowy_3.jfif`, `snowy_4.jfif`, `snowy_5.jfif`],
+  //     "Drizzle" : [`drizzle.jfif`, `drizzle_2.jpg`, `drizzle_3.jfif`, `drizzle_4.jfif`, `drizzle_5.jfif`],
+  //     "Rain" : [`rain.jfif`, `rain_2.jfif`, `rain_3.jfif`, `rain_4.jfif`, `rain_5.jfif`],
+  //   }
+  // }
 
   
-  function importAll(r) {
-    let image = {};
-    r.keys().map((item, index) => { image[item.replace('./', '')] = r(item); });
-    return image;
-  }
+  // function importAll(r) {
+  //   let image = {};
+  //   r.keys().map((item, index) => { image[item.replace('./', '')] = r(item); });
+  //   return image;
+  // }
   
-  const images = importAll(require.context('./media', false, /\.(jfif|jpe?g|JPG)$/));
+  // const images = importAll(require.context('./media', false, /\.(jfif|jpe?g|JPG)$/));
 
-  function updateBackgroundImage() {
-    let random = Math.floor(Math.random() * 5);
-    if(changeBgImage.multipleConditions.includes(props.data.mainDescription)){
-      return changeBgImage.multipleConditionsImages[random];
-    } else {
-      if(changeBgImage.otherConditions.includes(props.data.mainDescription)){
-        return changeBgImage.otherConditionsImages[props.data.mainDescription][random];
-      }
-    }
-  }
+  // function updateBackgroundImage() {
+  //   let random = Math.floor(Math.random() * 5);
+  //   if(changeBgImage.multipleConditions.includes(props.data.mainDescription)){
+  //     return changeBgImage.multipleConditionsImages[random];
+  //   } else {
+  //     if(changeBgImage.otherConditions.includes(props.data.mainDescription)){
+  //       return changeBgImage.otherConditionsImages[props.data.mainDescription][random];
+  //     }
+  //   }
+  // }
 
     return( 
     <div
         className="one-day-temp-container"
-        style={{ backgroundImage: `url(${images[updateBackgroundImage()].default})`}}
+        // style={{ backgroundImage: `url(${images[updateBackgroundImage()].default})`}}
       >
         <p className="city" id="city">
           {props.data.city}
